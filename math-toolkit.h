a5ef3c8a1cd4ab20b1ca71b3732ded0491d2dbbf
Ram <jack81306@yahoo.com.tw>
2017-03-02 20:31:22 +0800

    firstOptimize:loop unrolling in subtract_vector and dot_product

--- math-toolkit.h
+++ math-toolkit.h
@@ -32,8 +32,11 @@ void add_vector(const double *a, const double *b, double *out)
 static inline
 void subtract_vector(const double *a, const double *b, double *out)
 {
-    for (int i = 0; i < 3; i++)
-        out[i] = a[i] - b[i];
+    /*for (int i = 0; i < 3; i++)
+        out[i] = a[i] - b[i];*/
+    out[0] = a[0] - b[0];
+    out[1] = a[1] - b[1];
+    out[2] = a[2] - b[2];
 }
 
 static inline
@@ -62,8 +65,11 @@ static inline
 double dot_product(const double *v1, const double *v2)
 {
     double dp = 0.0;
-    for (int i = 0; i < 3; i++)
-        dp += v1[i] * v2[i];
+    /* for (int i = 0; i < 3; i++)
+         dp += v1[i] * v2[i];*/
+    dp += v1[0] * v2[0];
+    dp += v1[1] * v2[1];
+    dp += v1[2] * v2[2];
     return dp;
 }
 
